19bbd593897bfbc741337fda63e3f459cc875431
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-10-13 09:57:32 -0700

    fully commented out on the code

--- base2.js
+++ base2.js
@@ -23,20 +23,35 @@ $(document).ready(function(){
 	    //blogText input is waiting for a click or return key 
 		//stroke to submit the value
 	$('#new-tiny-post').submit(function(e) {
+		//prevent default action of submit button
 		e.preventDefault();
+		//variable for the input provided by the user
 		var postContent = $('#blogText').val();
+		//variable for the date, the system just knows this
 		var date = new Date();
+		//get day of the week
 		var day = date.getDay();
+		//get the time of day
 		var time = date.getTime();
+		//var day retrieves the number value of the week day.  The array below helps it identify the corresponding day name
 		var dayNames = ['noday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+		//variable to display date in this form 12/12/2012
 		var displayDate = (date.getMonth()+1) + '/' + (date.getDate()) + '/' + date.getFullYear();
+		//run the post function OOP constructor and put the user input values through it
 		new Post(postContent);
+		//but only if there is content in the input field...
 	  	if (postContent) {
+	  		//if there is content that is submittd, prepend it to the top of the bottom div in its own new div
+	  		//italisize the count and date, make the content of the post bold
 			$("#published-post-list").prepend("<i>" + '<div class="list-group-item">' + count + "</i>" + "<br>" + "<b>" + postContent + "</b>" + "<br>" + "<i>" + (dayNames[date.getDay()]) + " " + displayDate + '</div>' + "</i>");
+			//each time we run this function, add one to the number of posts
 			count++;
+			//logs count to make sure its working
 			console.log(count);
 		}
+			//logs Post to make sure its working
 			console.log(Post);
+			//resets the input value after submission so the user does not need to delete the content before publishing a new post
 			$('#blogText').val('');
 	});
 });	
